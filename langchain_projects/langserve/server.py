751112a133f23d995afa8ecfb1c0e4ed87f65ad0
Gaurav Verma <rythm_grv@yahoo.co.in>
2024-10-08 18:16:01 +0530

    first commit

---
+++ langchain_projects/langserve/server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+
+# import os
+
+# os.environ["OPENAI_API_KEY"] = ""
+# OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# print(OPENAI_API_KEY)
+
+OPENAI_API_KEY = 'sk-proj-BbeUnYhDZB7S3ChDXx3IT3BlbkFJOhzg8WVVueggmktUE6cn'
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = ChatOpenAI(api_key=OPENAI_API_KEY)
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# 5. Adding chain route
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
